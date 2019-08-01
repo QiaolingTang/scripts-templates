7803dd3a45b03f6d0fed5922cf633fd7d32f7acb
qitang <qitang@redhat.com>
2019-08-01 17:05:54 +0800

    remove dc.yaml

--- generate-log.py
+++ generate-log.py
@@ -21,11 +21,12 @@ logger.addHandler(handler1)
 #logger.addHandler(handler2)
 
 
-logfile = './log.json'
-f = open(logfile, "r")
-if f.mode == "r":
-    logs = f.read()
+#logfile = './log.json'
+#f = open(logfile, "r")
+#if f.mode == "r":
+#    logs = f.read()
 
+logs = '{"message": "MERGE_JSON_LOG=true", "testcase": "logging-test", "level": "debug"," Layer1": "layer1 0", "layer2": {"name":"Layer2 1", "tips":"decide by PRESERVE_JSON_LOG"}, "StringNumber":"10", "Number": 10,"foo.bar":"dotstring","{foobar}":"bracestring","[foobar]":"bracket string", "foo:bar":"colonstring", "empty1":"", "empty2":{}}'
 if __name__ == "__main__":
     i = 0
     while True:
