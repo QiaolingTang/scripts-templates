56b4302acb0498961bbedfc4c5bb4b3f49191332
QiaolingTang <qitang@redhat.com>
2022-06-17 15:10:53 +0800

    update

--- generate-log.py
+++ generate-log.py
@@ -2,15 +2,16 @@
 import logging
 import logging.handlers
 import time
+from datetime import datetime
 
 logger = logging.getLogger("logger")
 
 handler1 = logging.StreamHandler()
 #handler2 = logging.FileHandler(filename="/var/log/generated/test.log")
 
-logger.setLevel(logging.DEBUG)
-handler1.setLevel(logging.DEBUG)
-#handler2.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+handler1.setLevel(logging.INFO)
+#handler2.setLevel(logging.INFO)
 
 #formatter = logging.Formatter("time=%(asctime)s level=%(levelname)s msg=%(message)s")
 formatter = logging.Formatter("%(message)s")
@@ -21,14 +22,14 @@ logger.addHandler(handler1)
 #logger.addHandler(handler2)
 
 
-logfile = '/etc/generate-log/json.example'
-f = open(logfile, "r")
-if f.mode == "r":
-    logs = f.read()
+#logfile = '/etc/generate-log/json.example'
+#f = open(logfile, "r")
+#if f.mode == "r":
+#    logs = f.read()
 
-#logs = '{"message": "MERGE_JSON_LOG=true", "testcase": "logging-test", "level": "debug"," Layer1": "layer1 0", "layer2": {"name":"Layer2 1", "tips":"decide by PRESERVE_JSON_LOG"}, "StringNumber":"10", "Number": 10,"foo.bar":"dotstring","{foobar}":"bracestring","[foobar]":"bracket string", "foo:bar":"colonstring", "empty1":"", "empty2":{}}'
+logs = '{"message": "MERGE_JSON_LOG=true", "testcase": "logging-test", "level": "info"," Layer1": "layer1 0", "layer2": {"name":"Layer2 1", "tips":"decide by PRESERVE_JSON_LOG"}, "StringNumber":"10", "Number": 10,"foo.bar":"dotstring","{foobar}":"bracestring","[foobar]":"bracket string", "foo:bar":"colonstring", "empty1":"", "empty2":{}}'
 if __name__ == "__main__":
     i = 0
     while True:
-        logger.debug(logs)
-        time.sleep(2)
+        logger.info("{\"time: \"%s\", "+logs+"\"}", datetime.utcnow())
+        time.sleep(0.5)
